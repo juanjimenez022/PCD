3f45bc30e79bd8c05be9dc327acdab0c21e18461
jujis <jujis@jujise96-laptop>
2023-10-17 10:48:22 +0200

    Practica 1 PCD

---
+++ src/practica1_pcd/iPila.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package practica1_pcd;
+
+/**
+ *
+ * @author jujis
+ */
+public interface iPila {
+
+    public int GetNum();
+
+    public void Apila(Object obj) throws Exception;
+
+    public Object Desapila() throws Exception;
+
+    public Object Primero() throws Exception;
+}
